db5929fdb418c8cd2744b730a3057a9f8de7f112
USER <user@AL02529596-83.local>
2025-04-25 11:38:56 +0900

    일렉트론 1차 작업 완료

---
+++ FE/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true, // Node.js 통합 활성화
+    },
+  });
+
+  // index.html 파일 로드
+  mainWindow.loadFile('index.html');
+
+  // 창이 닫힐 때 참조 제거
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
